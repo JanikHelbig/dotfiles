c9e54f75788740a1aafe8036d3c7f632ef4c3af0
Janik Helbig <helbigjanik@gmail.com>
2023-09-13 01:06:26 +0200

    Add deployment script and manifests, add basic .gitconfig

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+symlinkFile() {
+    filename="$SCRIPT_DIR/$1"
+    destination="$HOME/$2/$1"
+
+    mkdir -p $(dirname "$destination")
+
+    if [ ! -L "$destination" ]; then
+	if [ -e "$destination" ]; then
+	    echo "[ERROR] $destination exists but is not a symlink." && exit 1
+	else
+	    ln -s "$filename" "$destination"
+	    echo "[OK] $filename -> $destination"
+	fi
+    else
+	echo "[WARNING] $filename already symlinked."
+    fi
+}
+
+deployManifest() {
+    for row in $(cat $SCRIPT_DIR/$1); do
+	filename=$(echo $row | cut -d \| -f 1)
+	operation=$(echo $row | cut -d \| -f 2)
+	destination=$(echo $row | cut -d \| -f 3)
+
+	case $operation in
+	    symlink)
+		symlinkFile $filename $destination
+		;;
+
+	    *)
+		echo "[WARNING] Unkown operation $operation. Skipping..."
+		;;
+	esac
+    done
+}
+
+echo "--- Common configs ---"
+deployManifest MANIFEST
+
+echo "--- MacOS configs ---"
+deployManifest MANIFEST.macos
