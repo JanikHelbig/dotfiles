e4f601c6ac5c91dd2d40dbf020210528085e349f
Janik Helbig <helbigjanik@gmail.com>
2023-09-14 00:22:00 +0200

    Fix whitespace in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,32 +9,32 @@ symlinkFile() {
     mkdir -p $(dirname "$destination")
 
     if [ ! -L "$destination" ]; then
-	if [ -e "$destination" ]; then
-	    echo "[ERROR] $destination exists but is not a symlink." && exit 1
-	else
-	    ln -s "$filename" "$destination"
-	    echo "[OK] $filename -> $destination"
-	fi
+        if [ -e "$destination" ]; then
+            echo "[ERROR] $destination exists but is not a symlink." && exit 1
+        else
+            ln -s "$filename" "$destination"
+            echo "[OK] $filename -> $destination"
+        fi
     else
-	echo "[WARNING] $filename already symlinked."
+        echo "[WARNING] $filename already symlinked."
     fi
 }
 
 deployManifest() {
     for row in $(cat $SCRIPT_DIR/$1); do
-	filename=$(echo $row | cut -d \| -f 1)
-	operation=$(echo $row | cut -d \| -f 2)
-	destination=$(echo $row | cut -d \| -f 3)
+        filename=$(echo $row | cut -d \| -f 1)
+        operation=$(echo $row | cut -d \| -f 2)
+        destination=$(echo $row | cut -d \| -f 3)
 
-	case $operation in
-	    symlink)
-		symlinkFile $filename $destination
-		;;
+        case $operation in
+            symlink)
+                symlinkFile $filename $destination
+                ;;
 
-	    *)
-		echo "[WARNING] Unkown operation $operation. Skipping..."
-		;;
-	esac
+            *)
+                echo "[WARNING] Unkown operation $operation. Skipping..."
+                ;;
+        esac
     done
 }
 
