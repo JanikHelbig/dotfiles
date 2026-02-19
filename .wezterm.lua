99b6dd0dfc4ee102c8c4dc551a41a7c3831fbb1c
Janik Helbig <helbigjanik@gmail.com>
2026-02-19 01:20:00 +0100

    Switch to single file nvim config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,26 +1,26 @@
 local wezterm = require 'wezterm'
 
-local config = {}
+local config =  wezterm.config_builder()
 
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
-
---  config.front_end = "WebGpu"
+config.front_end = "WebGpu"
 config.freetype_load_target = 'Light'
 
 -- Appearance Settings
 config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = false
+config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = { left = 11, right = 11, top = 11, bottom = 11 }
 config.window_decorations = 'RESIZE|INTEGRATED_BUTTONS'
 -- config.window_background_opacity = 0.95
-config.color_scheme = 'Catppuccin Frappe'
+config.color_scheme = 'Catppuccin Mocha'
 
 -- Font Settings
-config.font = wezterm.font 'JetBrains Mono'
--- config.font = wezterm.font 'Cascadia Code NF'
-config.font_size = 11.0
+-- config.font = wezterm.font {
+--     family = 'Monaspace Neon NF',
+--     harfbuzz_features = { 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'ss09', 'ss10' }
+-- }
+config.font = wezterm.font 'JetBrainsMono NFM'
+config.font_size = 12.0
+-- config.line_height = 1.2
 
 -- Platform Specific Settings
 function string:contains(value)
@@ -29,7 +29,8 @@ end
 
 local platform = wezterm.target_triple
 if platform:contains 'windows' then
-    config.default_prog = { 'pwsh', '--nologo' }
+    config.default_prog = { 'nu' }
+    -- config.default_prog = { 'pwsh', '--nologo' }
     -- config.window_decorations = 'RESIZE'
 elseif platform:contains 'apple' then
 else
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,154 @@
-require("config.options")
-require("config.lazy")
-require("config.godot")
+--- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+      "git", "clone", "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local is_windows = jit.os == 'Windows'
+
+--- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+--- Options
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.scrolloff = 8
+
+if is_windows then
+    vim.o.shell = 'pwsh'
+    vim.o.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command'
+end
+
+--- Plugins
+require("lazy").setup({
+    spec = {
+        {
+            "neovim/nvim-lspconfig"
+        },
+        {
+            "mason-org/mason.nvim",
+            opts = {},
+        },
+        {
+            "mason-org/mason-lspconfig.nvim",
+            dependencies = {
+                "mason-org/mason.nvim",
+                "neovim/nvim-lspconfig",
+            },
+            opts = {
+                ensure_installed = {
+                    "lua_ls",
+                },
+            },
+        },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            opts = {
+                ensure_installed = { "lua", "vimdoc", "markdown" },
+                highlight = { enable = true },
+            },
+        },
+        {
+            'saghen/blink.cmp',
+            version = '1.*',
+            opts = {
+                keymap = {
+                    preset = 'default'
+                },
+                sources = {
+                    default = { "lazydev", 'lsp', 'path', 'snippets', 'buffer' },
+                    providers = {
+                        lazydev = {
+                            name = "LazyDev",
+                            module = "lazydev.integrations.blink",
+                            score_offset = 100,
+                        },
+                    },
+                },
+            },
+            opts_extend = { 'sources.default' },
+        },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
+        {
+            'folke/which-key.nvim',
+            event = 'VeryLazy',
+            opts = {
+                preset = 'modern',
+            },
+            keys = {
+                {
+                    '<leader>?',
+                    function()
+                        require('which-key').show({ global = false })
+                    end,
+                    desc = 'Buffer Local Keymaps (which-key)',
+                }
+            }
+        },
+        {
+            'folke/snacks.nvim',
+            priority = 1000,
+            lazy = false,
+            opts = {
+                -- explorer = { enabled = true },
+                picker = { enabled = true },
+                terminal = {
+                    enabled = true,
+                    shell = is_windows and { 'pwsh', '-NoLogo' } or vim.o.shell,
+                },
+            },
+            keys = {
+                { '<leader><space>',    function() Snacks.picker.smart() end,   desc = 'Smart Find Files' },
+                { '<leader>e',          function() Snacks.explorer() end,       desc = 'File Explorer' },
+                -- Other
+                { '<C-/>',              function() Snacks.terminal() end,       desc = 'Toggle Terminal' },
+                { '<C-_>',              function() Snacks.terminal() end,       desc = 'which_key_ignore' },
+            },
+        },
+        {
+            'catppuccin/nvim',
+            name = 'catppuccin',
+            lazy = false,
+            priority = 1000,
+            opts = {
+                flavour = 'mocha',
+                auto_integrations = true,
+            },
+        },
+    },
+    install = { colorscheme = { "catppuccin" } },
+    checker = { enabled = true },
+})
+
+vim.cmd.colorscheme 'catppuccin'
+
+-- Godot LSP connection
+local root = vim.fs.root(0, 'project.godot')
+if root then
+    local pipe = is_windows and '\\\\.\\pipe\\godot-server' or root .. '/server.pipe'
+    if not vim.tbl_contains(vim.fn.serverlist(), pipe) then
+        vim.fn.serverstart(pipe)
+    end
+end
+
+vim.lsp.enable('gdscript')
